b2554a4a99621977328e7ceb939641022e166161
Jurayevkh <jurayevkh@gmail.com>
2024-01-03 23:49:23 +0500

    generated js code from ts

--- dist/typescript-basic/main.js
+++ dist/typescript-basic/main.js
@@ -3,4 +3,17 @@ let age = 23;
 if (age > 20)
     age += 10;
 console.log(age);
+let num = 36345634;
+let text = "Type annotate";
+let nothing;
+function displayParameter(parameter) {
+    console.log(parameter);
+}
+function displayParameter2(parameter) {
+    console.log(parameter);
+}
+let arrayInJS = [1, 3, 12, "23"];
+let arrayInTypescript = [1, 2, "2"];
+let array = [];
+let user = [1, "Rustambek"];
 //# sourceMappingURL=main.js.map
