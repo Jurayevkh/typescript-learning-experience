b3c855a83c78a573a4c640119e094daec233526a
Jurayevkh <jurayevkh@gmail.com>
2024-01-03 23:49:03 +0500

    any , arrays , tuples , js and ts built in types

--- typescript-basic/main.ts
+++ typescript-basic/main.ts
@@ -11,3 +11,47 @@ console.log(age);
 
 let num:number=36_345_634;//you can use underscore in numbers to be more readable
 let text="Type annotate";//we don't must annotate type of variable compiler of typescript can detect type of variab.
+let nothing;//but what if we don't give value to our variable , in such cases typescript assumes that this variable as any
+
+//The type Any
+//In Typescript Any is built in type that means any kind of values. If we declare value but don't initialize
+//typescript compiler assumes that this variable type of any , but this is against of whole idea of using
+//cause typescript's purpose is type safety , and this work lose main feature of typescript. Here another example for understand type any:
+
+function displayParameter(parameter){ //here we have compilation error that means we don't give a type of parameter
+    console.log(parameter);
+}   
+//we have two options for solve this error
+//1.Give type any to parameter:
+function displayParameter2(parameter:any){
+    console.log(parameter);
+}
+//2.Configure tsconfig.json type-checking section , "noImplicityAny" row to false 
+
+
+//Arrays
+//Ok let's rememeber how we declare arrays in JS:
+let arrayInJS=[1,3,12,"23"];//in Javascript we haven't type annotate and we can add any value to array
+//but thats too against to whole idea of typescript. Let me show you arrays in Typescript
+let arrayInTypescript:number[]=[1,2,"2"];//here we declare array in Typescript and annotate type of array after that
+                                         //we give other type value to this but we got a error here.
+//but what if we don't annotate type in Typescript
+let array=[]; //here our array gets type an any
+
+//Tuples
+let user:[number,string]=[1,"Rustambek"];//tuples in Typescript ,we give length types and we must fill with values
+                                         //in length of types and with that types we can't add other values 
+
+//Javascript built-in types
+  //number
+  //string
+  //boolean
+  //null
+  //undefined
+  //object
+//And Typescript extends this list new types like:
+  //any
+  //unknown
+  //never
+  //enum
+  //tuple
