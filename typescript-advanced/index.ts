db9e7ee324afd6f03f165ab7744e85bbeec33157
Jurayevkh <jurayevkh@gmail.com>
2024-01-05 02:31:40 +0500

    interfaces in typescript

---
+++ typescript-advanced/index.ts
@@ -0,0 +1,13 @@
+//Interfaces
+//Interface is also interface like other programming languages ,typescript's interface like c# interface
+
+interface IUser{
+    firstname:string,
+    lastname:string,
+    age:number,
+    readonly email:string
+}
+
+//and we can also take object from interfaces
+let mirasil:IUser={firstname:'Mirasil',lastname:'Temirov',age:23,email:'mirasil@mail.com'}
+
